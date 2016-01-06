c6579d5a17522b7993a450a8042b9f3f0d4979ba
Sean Boose <seanboose@gmail.com>
2016-01-06 13:36:17 -0600

    Removed flicker from smooth scrolling

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ function smoothScroll(loc) {
 	$('html, body').animate({
 		scrollTop: $(loc).offset().top
 	}, 2000);
+	return false;
 };
 
 var main = function() {
 
-	$('.scroll').click(function() {
+	$('.scroll').click(function(e) {
+		e.preventDefault();
 		smoothScroll($(this).attr('href'));
 	});
 };
