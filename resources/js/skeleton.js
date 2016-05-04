d5b742b627076ac5d4c377ee3a12df39c3a6c5af
Sean Boose <seanboose@gmail.com>
2016-05-04 14:48:16 -0500

    Screen-to-world mouse ray implemented.

--- resources/js/skeleton.js
+++ resources/js/skeleton.js
@@ -4,19 +4,90 @@ var renderer = new THREE.WebGLRenderer({ antialias: true });
 var scene = new THREE.Scene;
 var bone_material = new THREE.LineBasicMaterial({color: 0xff00ff});
 
+
+var camera_distance = 2;
+var rotation_speed = 0.05;
+var eye  = new THREE.Vector3(0, 0.1, camera_distance);
+var up   = new THREE.Vector3(0, 1, 0);
+var look = new THREE.Vector3(0, 0, 1);
+var tangent = new THREE.Vector3().crossVectors(up, look);
+var orientation = new THREE.Matrix3();
+orientation.set(
+	tangent.x, up.x, look.x,
+	tangent.y, up.y, look.y,
+	tangent.z, up.z, look.z);
+
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 10000);
+camera.position.set(0,0,camera_distance);
+camera.lookAt(look);
+
+
+var last_x = 0;
+var last_y = 0;
+var current_x = 0;
+var current_y = 0;
+var mouse_ray = new THREE.Vector3();
+
+var mousedown = false;
+document.body.onmousedown = function() { 
+  mousedown = true;
+}
+document.body.onmouseup = function() {
+  mousedown = false;
+}
+
+document.onmousemove = handleMouseMove;
+function handleMouseMove(event) {
+
+	last_x = current_x;
+	last_y = current_y;
+	current_x = event.pageX;
+	current_y = event.pageY;
+	console.log("Mouse moved: " + current_x + " " + current_y);
+
+	var delta_x = current_x - last_x;
+	var delta_y = current_y - last_y;
+	if(Math.sqrt(delta_x * delta_x + delta_y * delta_y) < 1e-15) return;
+
+	var mouse_direction = new THREE.Vector3(delta_x, delta_y, 0);
+	var mouse_start = new THREE.Vector2(last_x, last_y);
+	var mouse_end = new THREE.Vector2(current_x, current_y);
+
+	var viewport = new THREE.Vector4(0,0, window.innerWidth, window.innerHeight);
+
+	if(mousedown){
+		var axis_vector = new THREE.Vector3(0, mouse_directon.x, -mouse_directon.y);
+		var axis = new THREE.Vector3().multiplyVectors(orientation, axis_vector);
+		axis.normalize();
+
+		var rotation = new THREE.Matrix4().makeRotationAxis(axis, rotation_speed);
+
+		if(selected_bone != null){
+			selected_bone.si = selected_bone.si.multiply(rotation);
+		}
+	}
+	else {
+		var x =  2 * current_x / window.innerWidth - 1;
+		var y = -2 * current_y / window.innerHeight + 1;
+		var z = -1;
+
+		mouse_ray = new THREE.Vector3(x, y, z);
+		mouse_ray.unproject(camera);
+		mouse_ray.normalize();
+
+		var p0 = mouse_ray.clone();
+		var p1 = mouse_ray.clone().multiplyScalar(1000);
+		var geom = new THREE.Geometry();
+		geom.vertices.push(p0);
+		geom.vertices.push(p1);
+		var line = new THREE.Line(geom, bone_material);
+		scene.add(line);
+	}
+}
+
 function onBoneFileLoaded(){
 	bone_text = this.responseText;
 }
-
-// function mat4XVec4(m, v){
-// 	var v0 = m[0]*v[0] + m[4]*v[1] + m[8]*v[2]  + m[12]*v[3];
-// 	var v1 = m[1]*v[0] + m[5]*v[1] + m[9]*v[2]  + m[13]*v[3];
-// 	var v2 = m[2]*v[0] + m[6]*v[1] + m[10]*v[2] + m[14]*v[3];
-// 	var v3 = m[3]*v[0] + m[7]*v[1] + m[11]*v[2] + m[15]*v[3];
-
-// 	// return vec4.fromValues(v0,v1,v2,v3);
-// 	return new THREE.Vector4(v0, v1, v2, v3);
-// }
 
 var first = false;
 function drawBone(bone, trans){
@@ -75,10 +146,10 @@ function init(){
 	renderer.setSize(width, height);
 	document.body.appendChild(renderer.domElement);
 	 
-	// Create camera
-	var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
-	camera.position.set(0,0,2);
-	camera.lookAt(new THREE.Vector3(0,0,0));
+	// // Create camera
+	// var camera = new THREE.PerspectiveCamera(45, width / height, 0.1, 10000);
+	// camera.position.set(0,0,camera_distance);
+	// camera.lookAt(look);
 
 	scene.add(camera);
 
